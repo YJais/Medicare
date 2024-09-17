0ef45158e335eb4dff9a9ac85b581afd471f2ed5
Yash <jaiswalyashraj2004@gmail.com>
2024-09-18 04:20:15 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const getconnect= require('./dbconnect')
+const admin= require('./dbconnect')
 const app=express()
 app.set("view engine","ejs");
 app.use(express.static(__dirname+'/public'))
@@ -13,9 +14,10 @@ app.get("/signup",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.render("login",{errors:{}});
 });
-// app.get("/dashboard"(req,res)=>{
-//     res
-// })
+app.get("/login_admin",(req,res)=>{
+    res.render("login_admin");
+});
+
 app.post("/signup",async(req,res)=>{
     let fname=req.body.firname;
     let mname=req.body.midname;
@@ -29,17 +31,17 @@ app.post("/signup",async(req,res)=>{
     if (userWithSameUsername) {
         errors.username = "Username is already taken.";
     }
-
+    
     let userWithSameEmail = await collection.findOne({ usremail: uemail });
     if (userWithSameEmail) {
         errors.email = "Email is already registered.";
     }
-
+    
     if (Object.keys(errors).length > 0) {
         console.log("Error detected: ", errors);
         return res.render("signup", { errors });
     }    
-
+    
     let r= await collection.insertOne({
         firname:fname,
         midname:mname,
@@ -49,11 +51,16 @@ app.post("/signup",async(req,res)=>{
         usrpass:upass
     });
     console.log("New user data inserted.");
-    res.render("login", { success: "You have successfully signed up! Please log in." });
+    res.redirect("/login");
 });
-app.get("/dashboard",async(req,res)=>{
-    let uemail=req.query.usremail;
-    let upass=req.query.usrpass;
+
+app.get("login",(req,res)=>{
+    res.render("login",{ success: "You have successfully signed up! Please log in." })
+});
+
+app.post("/login",async(req,res)=>{
+    let uemail=req.body.usremail;
+    let upass=req.body.usrpass;
     let collection=await getconnect();
     let errors={};
     let user=await collection.findOne({usremail:uemail});
@@ -62,17 +69,30 @@ app.get("/dashboard",async(req,res)=>{
         console.log("E-mail not Found");
         return res.render("login",{errors});
     }
-    if(user.usrpass!==upass){
-        errors.password="Password not Found";
-        console.log("Password not Found");
-        return res.render("login",{errors})
-    }
     console.log("Login Successful. Redirecting to dashboard");
-    res.render("dashboard",{success:"You have successfully logged in!"})
+    res.redirect("/dashboard");
 });
+
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard",{ success: "You have successfully logged in!" })
+});
+
 app.get("/getappointment",(req,res)=>{
     res.render("appointment");
-})
+});
+
+app.post("/login_adminbtn",async(req,res)=>{
+    let aname=req.body.adname;
+    let apass=req.body.adpass;
+    let collection=await admin();
+    console.log("Login Successful. Redirecting to dashboard");
+    res.redirect("/dashboard_admin");
+});
+
+app.get("/dashboard_admin",(req,res)=>{
+    res.render("dashboard_admin",{ success: "You have successfully logged in!" })
+});
+
 app.listen(5000,()=>{
     console.log("Listening");
 })
