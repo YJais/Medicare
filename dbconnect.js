9fde4616ded8970b8fcf46c93cb82981ec0efbef
Yash <jaiswalyashraj2004@gmail.com>
2024-09-19 18:34:48 +0530

    Initial commit

--- dbconnect.js
+++ dbconnect.js
@@ -1,17 +1,39 @@
 const {MongoClient}=require("mongodb");
-async function getconnect(){
+async function getcollection(collectionname){
         const client=new MongoClient("mongodb://localhost:27017/medicare");
          let con=await client.connect();
          let db=con.db("medicare");
-         let collection=db.collection("clientdata");
+         let collection=db.collection(collectionname);
          return collection;
+}
+async function getconnect(){
+          return await getcollection("clientdata");
 }
 async function admin(){
-        const client=new MongoClient("mongodb://localhost:27017/medicare");
-         let con=await client.connect();
-         let db=con.db("medicare");
-         let collection=db.collection("admindata");
-         return collection;
+          return await getcollection("admindata");
 }
-module.exports=getconnect;
-module.exports=admin;
+async function doctor(){
+          return await getcollection("doctordata");
+}
+async function patient(){
+          return await getcollection("patientdata");
+}
+// async function doctor(){
+//         const client=new MongoClient("mongodb://localhost:27017/medicare");
+//          let con=await client.connect();
+//          let db=con.db("medicare");
+//          let collection=db.collection("doctordata");
+//          return collection;
+// };
+// async function patient(){
+//         const client=new MongoClient("mongodb://localhost:27017/medicare");
+//          let con=await client.connect();
+//          let db=con.db("medicare");
+//          let collection=db.collection("patientdata");
+//          return collection;
+// };
+
+module.exports={getconnect,admin,doctor,patient};
+// module.exports=admin;
+// module.exports=doctor;
+// module.exports=patient;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const getconnect= require('./dbconnect')
-const admin= require('./dbconnect')
-const app=express()
+const {getconnect,admin,doctor,patient}= require('./dbconnect');
+// const admin= require('./dbconnect');
+const app=express();
 app.set("view engine","ejs");
 app.use(express.static(__dirname+'/public'))
 app.use(express.urlencoded({ extended: true }));
@@ -11,12 +11,10 @@ app.get("/",(req,res)=>{
 app.get("/signup",(req,res)=>{
     res.render("signup",{errors:{}});
 });
-app.get("/login",(req,res)=>{
-    res.render("login",{errors:{}});
-});
-app.get("/login_admin",(req,res)=>{
-    res.render("login_admin");
-});
+// app.get("/login",(req,res)=>{
+//     res.render("login",{errors:{}});
+// });
+
 
 app.post("/signup",async(req,res)=>{
     let fname=req.body.firname;
@@ -69,6 +67,12 @@ app.post("/login",async(req,res)=>{
         console.log("E-mail not Found");
         return res.render("login",{errors});
     }
+    if (user.usrpass !== upass) {
+        errors.password = "Incorrect password.";
+        console.log("Incorrect password.");
+        return res.render("login", { errors });
+    }
+
     console.log("Login Successful. Redirecting to dashboard");
     res.redirect("/dashboard");
 });
@@ -79,6 +83,12 @@ app.get("/dashboard",(req,res)=>{
 
 app.get("/getappointment",(req,res)=>{
     res.render("appointment");
+});
+
+//admin
+
+app.get("/login_admin",(req,res)=>{
+    res.render("login_admin");
 });
 
 app.post("/login_adminbtn",async(req,res)=>{
@@ -93,6 +103,110 @@ app.get("/dashboard_admin",(req,res)=>{
     res.render("dashboard_admin",{ success: "You have successfully logged in!" })
 });
 
+app.get("/docdata",async (req,res)=>{
+    let collection=await doctor();
+    let records=await collection.find({}).toArray();
+    // console.log(records);
+    res.render("docdata",{records});
+});
+app.get("/insertdocdata",(req,res)=>{
+    res.render("insertdocdata");
+    
+});
+app.post("/inst",async (req,res)=>{
+    let d_id=req.body.doc_id;
+    let d_name=req.body.doc_name;
+    let d_desig=req.body.doc_desig;
+    let d_sal=req.body.doc_sal;
+    let d_dept=req.body.doc_dept;
+    let collection=await doctor();
+    let r=await collection.insertOne({
+        doc_id:d_id,
+        doc_name:d_name,
+        doc_desig:d_desig,
+        doc_sal:d_sal,
+        doc_dept:d_dept
+    });
+    let records=await collection.find({}).toArray();
+    res.render("docdata",{records});
+});
+app.get("/deletedocdata",(req,res)=>{
+    res.render("deletedocdata");
+});
+
+app.get("/del",async (req,res)=>{
+    let d_id=req.query.doc_id;
+    let collection=await doctor();
+    let r=await collection.deleteOne({doc_id:d_id});
+    let records=await collection.find({}).toArray();
+	   res.render("docdata",{records});
+});
+
+// app.get("/updatedocdata",(req,res)=>{
+//     res.render("updatedocdata");
+// });
+
+// app.post("/updateres",async (req,res)=>{
+//    let d_id=req.body.d_id;
+//    let d_name=req.body.d_name;
+//    let d_desig=req.body.d_name;
+//    let d_sal=req.body.d_sal;
+//    let collection=await doctor();
+//    let r=await collection.updateOne({id:d_id},{$set:{name:d_name,designation:d_desig,salary:d_sal}});
+//    let records=await collection.find({}).toArray();
+//    res.render("docdata",{records});
+// });
+
+// app.get("/updatedocdata", async (req, res) => {
+//     let doc_id = req.query.d_id; // Assuming you're passing the doctor ID via query parameter
+//     let collection = await doctor(); // Access the 'doctors' collection
+
+//     // Find the doctor record by ID
+//     let rec = await collection.findOne({ doc_id: doc_id });
+
+//     // Check if the doctor was found
+//     if (!rec) {
+//         return res.status(404).send("Doctor not found.");
+//     }
+
+//     // Pass the doctor's record to the EJS template
+//     res.render("updatedocdata", { rec });
+// });
+
+app.get("/updatedocdata", async (req, res) => {
+    let doc_id = req.query.d_id; // Assuming you're passing the doctor ID via query parameter
+    let collection = await doctor(); // Access the 'doctors' collection
+
+    // Find the doctor record by ID
+    let rec = await collection.findOne({ doc_id: doc_id });
+
+    // Check if the doctor was found
+    if (!rec) {
+        return res.status(404).send("Doctor not found.");
+    }
+
+    // Pass the doctor's record to the EJS template
+    res.render("updatedocdata", { rec });
+});
+
+app.post("/updateres", async (req, res) => {
+    let d_id = req.body.d_id;
+    let d_name = req.body.d_name;
+    let d_desig = req.body.d_desig;
+    let d_sal = req.body.d_sal;
+    let collection = await doctor();
+
+    // Update doctor details
+    await collection.updateOne({ doc_id: d_id }, { $set: { doc_name: d_name, doc_desig: d_desig, doc_sal: d_sal } });
+
+    // Fetch updated records
+    let records = await collection.find({}).toArray();
+    res.render("docdata", { records });
+});
+
+
+        
+//Server Port
 app.listen(5000,()=>{
     console.log("Listening");
 })
